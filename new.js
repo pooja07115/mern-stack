df40f0c69d2ade30c3a7eef8d7e844e3f1d72d22
pooja07115 <pooja13705@gmail.com>
2025-01-24 12:22:51 +0530

    prompt game

---
+++ new.js
@@ -0,0 +1,13 @@
+let user={
+    name:"Pooja",
+    age:19,
+    "is singer":true,
+}
+console.log(user);
+for(let key in user){
+    console.log(key+" : "+user[key]);
+}
+const array=[1,2,3,4,5]
+for(let value of array){
+    console.log(value);
+} 
